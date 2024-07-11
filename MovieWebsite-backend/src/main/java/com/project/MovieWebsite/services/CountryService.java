629c45e60a5b28bf6a18e39f1e3f036022e04aee
Huynh Linh <huynhvanlinh1062003@gmail.com>
2024-07-11 15:29:25 +0700

    fix country

--- MovieWebsite-backend/src/main/java/com/project/MovieWebsite/services/CountryService.java
+++ MovieWebsite-backend/src/main/java/com/project/MovieWebsite/services/CountryService.java
@@ -6,13 +6,43 @@ import java.util.List;
 
 public interface CountryService {
 
-    Country createCountry(CountryDTO countryDTO);
+    /**
+ * Creates a new country entity based on the provided CountryDTO object.
+ *
+ * @param countryDTO Data Transfer Object containing the details of the country to be created.
+ * @return The created Country entity.
+ */
+Country createCountry(CountryDTO countryDTO);
 
-    Country getCountryById(int countryId);
+/**
+ * Retrieves a country entity by its unique identifier.
+ *
+ * @param countryId The unique identifier of the country to be retrieved.
+ * @return The Country entity with the specified ID.
+ */
+Country getCountryById(int countryId);
 
-    List<Country> getAllCountry();
+/**
+ * Retrieves a list of all country entities.
+ *
+ * @return A list of all Country entities.
+ */
+List<Country> getAllCountry();
 
-    Country updateCountry(int countryId, CountryDTO countryDTO);
+/**
+ * Updates the details of an existing country entity based on the provided CountryDTO object.
+ *
+ * @param countryId The unique identifier of the country to be updated.
+ * @param countryDTO Data Transfer Object containing the updated details of the country.
+ * @return The updated Country entity.
+ */
+Country updateCountry(int countryId, CountryDTO countryDTO);
 
-    void deleteCountry(int countryId);
+/**
+ * Deletes a country entity by its unique identifier.
+ *
+ * @param countryId The unique identifier of the country to be deleted.
+ */
+void deleteCountry(int countryId);
+
 }
